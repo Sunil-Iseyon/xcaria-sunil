cf4c819b6b2814366271ef1fa74fe65068e95b1b
Manasi-007 <Manasi.Behera@iseyon.com>
2024-07-10 14:21:56 +0530

    Showcase page added

--- src/app/(routes)/showcase/animatedText.jsx
+++ src/app/(routes)/showcase/animatedText.jsx
@@ -1,73 +1,11 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-// import {createContext} from 'react';
 
 const AnimatedTextWord = ({text1}) =>{
   // const words = text.split(" ");
   const letters1 = Array.from(text1);
   // const letters2 = Array.from(text2);
-
-  // const container = {
-  //   hidden: { opacity: 0 },
-  //   visible: (i = 0.3) => ({
-  //     opacity: 1,
-  //     transition: { staggerChildren: 0.2, delayChildren: 0.4 * i },
-  //   }),
-  // };
-  const container2 = {
-    hidden: { opacity: 0 },
-    visible: (i = 1.85) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.2, delayChildren: 1 * i },
-    }),
-  };
-
-  // const child = {
-  //   visible: {
-  //     opacity: 1,
-  //     x:0,
-  //     y: 0,
-  //     transition: {
-  //       type: "spring",
-  //       damping: 12,
-  //       stiffness: 100,
-  //     },
-  //   },
-  //   hidden: {
-  //     opacity: 0,
-  //     x: -20,
-  //     y: 10,
-  //     transition: {
-  //       type: "spring",
-  //       damping: 12,
-  //       stiffness: 100,
-  //     },
-  //   },
-  // };
-
-  // const child2 = {
-  //   visible: {
-  //     opacity: 1,
-  //     x:0,
-  //     y: 0,
-  //     transition: {
-  //       type: "spring",
-  //       damping: 12,
-  //       stiffness: 100,
-  //     },
-  //   },
-  //   hidden: {
-  //     opacity: 0,
-  //     x: -20,
-  //     y: 10,
-  //     transition: {
-  //       type: "spring",
-  //       damping: 12,
-  //       stiffness: 100,
-  //     },
-  //   },
-  // };
   const fadeInUpAnimation = {
     hidden: {
         opacity: 0,
@@ -81,37 +19,26 @@ const AnimatedTextWord = ({text1}) =>{
             staggerChildren: 0.07,
             opacity: { duration: 3 },
             ease: "easeTn",
-            duration: .8,
+            duration: 0.1,
         },
     },
 };
 
 
   return(
-    <motion.div
+    <motion.div 
     initial="hidden"
     animate="show"
     variants={fadeInUpAnimation}
-    // style={{ overflow: "hidden", display: "flex", fontSize: "2rem" }}
-      // variants={container}
-      // initial="hidden"
-      // animate="visible"
-    
     >
        {letters1.map((letter, index) => (
         <motion.span variants={fadeInUpAnimation} key={index}
-        className='uppercase z-10 text-5xl sm:text-2xl md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-green-500 font-bold leading-tight tracking-tight'>
+        className='uppercase z-10 text-center text-5xl sm:text-2xl md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-green-500 font-bold leading-tight tracking-tight'>
           {letter === " " ? " " : letter}
         </motion.span>
       ))}
-      {/* {letters2.map((letter, index) => (
-        <motion.span variants={fadeInUpAnimation} key={index}
-        className='z-10 text-4xl sm:text-4xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-green-700 font-bold leading-tight tracking-tight'>
-          {letter === " " ? " " : letter}
-        </motion.span>
-      ))} */}
       <motion.div variants={fadeInUpAnimation}
-        className='text-white w-[750px] my-5 mx-auto text-sm'>
+        className='text-white w-[750px] my-5 mx-auto text-sm text-center'>
          <h3 > Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus placeat quos eum 
           laudantium doloremque, nam praesentium aspernatur ipsum</h3>
         </motion.div>
--- src/app/(routes)/showcase/page.tsx
+++ src/app/(routes)/showcase/page.tsx
@@ -6,13 +6,15 @@ import AnimatedTextWord from "./animatedText";
 import styles from "./page.module.css";
 import { data } from "./data";
 import Image from "next/image";
+import Link from "next/link";
 // import { motion } from "framer-motion";
 
-export default async function RemoteMdxPage() {
+export default function RemoteMdxPage() {
   // MDX text - can be from a local file, database, CMS, fetch, anywhere...
   // const res = await fetch('https://...')
   // const markdown = await res.text()
   // return <MDXRemote source={markdown} />
+
   // const fadeInUpAnimation = {
   //   hidden: {
   //     opacity: 0,
@@ -20,91 +22,55 @@ export default async function RemoteMdxPage() {
   //   },
   //   show: {
   //     opacity: 1,
-  //     delay: .8,
+  //     delay: .2,
   //     y: -80,
   //     transition: {
   //       staggerChildren: .5,
   //       duration: .8,
-  
+
   //     },
   //   },
   // };
   return (
     <>
-      <div className="flex items-center mt-32 justify-center w-screen ">
-      {/* <Particle id="part"/> */}
-      {/* <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      /> */}
-      <div className="text-center">
+      <div className="flex items-center mt-36 justify-center w-screen ">
+        {/* <Particle id="part"/> */}
+        {/* <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/> */}
           <AnimatedTextWord text1="An AI tool to make your work smoother" />
-        </div>
-
+          
       </div>
-      <div className="h-screen w-screen flex flex-wrap gap-7 items-center justify-center mt-20 text-white text-center ">
-        {data.map((i) => (
-          <div
-          // initial='hidden'
-          // whileInView='show'
+      {/* cards  */}
+      <div className="h-screen w-screen flex flex-wrap gap-7 items-center justify-center mt-12 text-white text-center ">
+      {data.map((i) => (
+        <div
+          // initial="hidden"
+          // whileInView="show"
           // viewport={{ once: true }}
           // variants={fadeInUpAnimation}
-          className={`${styles.sect} space-y-4`}>
-            {/* image  */}
-            <div className="flex justify-center items-center ">
-              <div className='flex justify-center items-center bg-gradient-to-br from-cyan-500 to-green-400 h-16 w-16 rounded-xl' >
-                <Image  src={i.img} alt="Image is getting loaded" width={40} height={40}/>
-              </div>
-            </div>
-            {/* heading  */}
-            <div>
-              <h3 className=" text-xl">{i.heading}</h3>
-            </div>
-            {/* desc  */}
-            <div className={styles.descp}>
-              <p className="text-xs">{i.descp}</p>
-
-              {/* button  */}
-              <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00ff77_0%,#04f9d0_50%,#000000_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              View More
-              </span> </button> 
+          className={`${styles.container} space-y-4`}
+        >
+          {/* image  */}
+          <div className="flex justify-center items-center ">
+            <div className="flex justify-center items-center bg-gradient-to-br from-cyan-500 to-green-400 h-16 w-16 rounded-xl">
+              <Image src={i.img} alt="Image is getting loaded" width={40} height={40}/>
             </div>
           </div>
-        ))}
+          {/* heading  */}
+          <div><h3 className=" text-xl font-bold  text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-green-500">{i.heading}</h3> </div>
+          {/* desc  */}
+          <div className={styles.descp}>
+            <p className="text-xs">{i.descp}</p>
+            {/* button  */}
+            <Link href="/" className={`${styles.view} relative inline-flex h-10 overflow-hidden rounded-full p-[1px] `}>
+              <span className={`${styles.span1} absolute inset-[-1000%] animate-[spin_2s_linear_infinite] `}/>
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 backdrop-blur-3xl">
+                View More
+              </span>
+            </Link>
+          </div>
+        </div>
+      ))}
       </div>
-      {/* <h3>{data[0].heading}</h3>
-              <p>{data[0].descp}</p> */}
-
-      {/* <div className={`${styles.insideBox}`}>
-            <div>
-              <h3>{data[1].heading}</h3>
-              <p>{data[1].descp}</p>
-            </div>
-          </div> */}
-
-      {/* <section className={styles.sect}>
-          <div>
-            <h3>{data[2].heading}</h3>
-            <p>{data[2].descp}</p>
-          </div>
-        </section> */}
-
-      {/* <nav className="w-[30%] h-[500px] space-y-5">
-          <div className={`${styles.insideBox}`}>
-            <div>
-              <h3>{data[3].heading}</h3>
-              <p>{data[3].descp}</p>
-            </div>
-          </div>
-          <div className={`${styles.insideBox} ${styles.iB4}`}>
-            <div>
-              <h3>{data[4].heading}</h3>
-              <p>{data[4].descp}</p>
-            </div>
-          </div>
-        </nav> */}
     </>
   );
 }
