fec216769e4ef8945e8546d385e6f90dbe2d82f4
Manasi-007 <Manasi.Behera@iseyon.com>
2024-08-22 17:33:48 +0530

    authorization added

--- src/app/(routes)/showcase/[showmore]/page.tsx
+++ src/app/(routes)/showcase/[showmore]/page.tsx
@@ -1,40 +1,46 @@
 import { datas } from "../datas";
 import ViewMorePage from "./show";
-import { createClient } from "@/src/utils/supabase/client";
+import { createClient } from "@/src/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 interface PageProps {
-  params: { showmore: string }; 
+  params: { showmore: string };
 }
 
 export default async function Page({ params }: PageProps) {
   const id = params.showmore;
-  const rawData = await fetch('https://xcaria-public.onrender.com/news');
+  // let usertype;
+  // if(id == '0' || id == '1' || id =='2' ){
+  //    usertype = 'client';
+  // }
+  // console.log("id= ",id);  
+  const rawData = await fetch('http://localhost:8080/news');
   
   if (!rawData.ok) {
     throw new Error('Failed to fetch the data!!!');
   }
 
   const data = await rawData.json();
-
+  
   const supabase = createClient()
-    const { data: activeSession } = await supabase.auth.getSession();
+  //   const { data: activeSession } = await supabase.auth.getSession();
 
-	if (!activeSession.session) {
-		return redirect("/auth");
-	}
+	// if (!activeSession.session) {
+	// 	return redirect("/auth");
+	// }
 
 	const { data: user } = await supabase.from("user").select("*").single();
 
-  if (user?.role !== "admin") {
+
+  if (user?.role === "user") {
     return(
        <h1 className='h-screen flex justify-center items-center'>Sorry You Don't have access to this page</h1>
       
     )
 	}
-  
 
   return (
     <ViewMorePage id={id} news={data} />
+    // <pre className="text-white mt-60 ml-80">{JSON.stringify(user,null,2)}</pre>
   );
 }
